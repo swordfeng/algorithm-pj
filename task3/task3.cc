bc307e078bb4a1d4761451efacc51fd056b2584b
Jianfeng Zhang <swordfeng123@gmail.com>
2017-06-02 22:19:03 +0800

    limit upper_bound, use savelog(log) to save log

--- task3/task3.cc
+++ task3/task3.cc
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <unordered_map>
 
 using namespace std;
 
@@ -12,86 +13,61 @@ enum OP {
     DEL = 2
 };
 
-const int NODELEN = 10000;
-
+#pragma pack(push, 1)
 struct Log {
-    OP op;
-    int x;
-    char c;
-    shared_ptr<Log> last;
-
-    void print();
-    void doo(vector<string> &list);
+    OP op : 2;
+    int x : 14;
+    char c : 2;
+    long long last : 35;
 };
-Log log_nil;
-const Log *LOG_NIL = &log_nil;
+#pragma pack(pop)
 
-void Log::print() {
-    if (this != LOG_NIL) {
-        last.print();
-        switch op {
-        case INS:
-            printf("INS %d %c\n", x, c);
-            break;
-        case SUB:
-            printf("SUB %d %c\n", x, c);
-            break;
-        case DEL:
-            printf("DEL %d\n", x);
-            break;
-        }
+const int NODELEN = 1000000;
+const int UB = 30000;
+const long long log_nil = 0;
+long long log_count = 0;
+
+long long savelog(Log log) {
+    return ++log_count;
+}
+
+char convchar(char c) {
+    switch (c) {
+        case 'A': return 0;
+        case 'C': return 1;
+        case 'G': return 2;
+        case 'T': return 3;
     }
-}
-void Log::doo(vector<string> &list) {
-    if (this == LOG_NIL) return;
-    switch op {
-    case INS:
-        list[x] = string(1, c) + list[x];
-        break;
-    case SUB:
-        string &orig = list[x];
-        orig[orig.size()-1] = c;
-        break;
-    case DEL:
-        string &orig := list[x];
-        orig.pop_back();
-        break;
-    }
-    last.doo(list)
-}
-vector<string> conv(string s) {
-    vector<string> r(s.size()+1);
-    for (int i = 0; i < s.size(); i++) r[i] = string(1, s[i]);
-    return r;
-}
-string unconv(vector<string> r) {
-    string s;
-    for (string &ss : r) s += ss;
-    return s;
+    exit(255);
 }
 
-
-shared_ptr<Log> ins(int x, char c, shared_ptr<Log> last) {
-    return make_shared<Log>({INS, x, c, last});
+long long ins(int x, char c, long long last) {
+    c = convchar(c);
+    Log log = {INS, x, c, last};
+    return savelog(log);
 }
-shared_ptr<Log> sub(int x, char c, shared_ptr<Log> last) {
-    return make_shared<Log>({SUB, x, c, last});
+long long sub(int x, char c, long long last) {
+    c = convchar(c);
+    Log log = {SUB, x, c, last};
+    return savelog(log);
 }
-shared_ptr<Log> del(int x, shared_ptr<Log> last) {
-    return make_shared<Log>({DEL, x, 0, last});
+long long del(int x, long long last) {
+    Log log = {DEL, x, 0, last};
+    return savelog(log);
 }
 
 struct PreWorkState {
-    shared_ptr<Log> log;
+    long long log;
     int times;
-}
+};
+void updatePreWork(int j, int i);
 
 struct Node {
-    shared_ptr<Log> log;
+    long long log;
     int times;
     // prework
     vector<PreWorkState> preWork;
-}
+};
 
 string a;
 vector<int> graph[NODELEN], prevgraph[NODELEN];
@@ -111,184 +87,204 @@ int doWork() {
     for (int j = 0; j < n; j++) {
         nodeseq[j] = j;
         nodes[j].preWork.resize(k + 1);
-        updatePreWork(j, 0)
+        updatePreWork(j, 0);
         nodes[j].times = nodes[j].preWork[k].times;
         nodes[j].log = nodes[j].preWork[k].log;
-        newnodes[j].times = -1;
+        newnodes[j].times = UB;
+        newnodes[j].log = log_nil;
     }
     // work
     for (int i = 1; i <= a.size(); i++) {
-        if (i % 100 == 0) {
-            fprintf(stderr, "%d\n", i);
-        }
+        fprintf(stderr, "%d\n", i);
         int pn = nodes[nodeseq[0]].times;
         for (int j : nodeseq) {
-//=================================================================================
             // flush queue
-            p := nodes[j].times
-            if p > pn + 1 {
-                nodeseqNext = append(nodeseqNext, pending...)
-                pending = pending[:0]
-                nodeseqNext = append(nodeseqNext, pendingNext...)
-                pendingNext = pendingNext[:0]
-            } else if p == pn + 1 {
-                nodeseqNext = append(nodeseqNext, pending...)
-                pending, pendingNext = pendingNext, pending[:0]
+            int p = nodes[j].times;
+            if (p > pn + 1) {
+                nodeseqNext.insert(nodeseqNext.end(), pending.begin(), pending.end());
+                pending.resize(0);
+                nodeseqNext.insert(nodeseqNext.end(), pendingNext.begin(), pendingNext.end());
+                pendingNext.resize(0);
+            } else if (p == pn + 1) {
+                nodeseqNext.insert(nodeseqNext.end(), pending.begin(), pending.end());
+                swap(pending, pendingNext);
+                pendingNext.resize(0);
             }
-            pn = p
+            pn = p;
             // prework
-            updatePreWork(j, i)
-            if nodes[j].preWork != nil {
-                if newnodes[j].times == -1 || nodes[j].preWork[k].times < newnodes[j].times {
-                    newnodes[j].times = nodes[j].preWork[k].times
-                    newnodes[j].log = nodes[j].preWork[k].log
+            updatePreWork(j, i);
+            if (nodes[j].preWork.size() > 0) {
+                if (nodes[j].preWork[k].times < newnodes[j].times) {
+                    newnodes[j].times = nodes[j].preWork[k].times;
+                    newnodes[j].log = nodes[j].preWork[k].log;
                 }
-                if nodes[j].preWork[k].times == i - k {
+                if (nodes[j].preWork[k].times == i - k) {
                     // must DEL (i-k) times and this is the real time, so all ops are DEL
                     // then we confirm that more operations are all DEL, so we do not need them
-                    nodes[j].preWork = nil
+                    nodes[j].preWork.resize(0);
+                    nodes[j].preWork.shrink_to_fit();
                 }
             }
             // del
-            if newnodes[j].times == -1 || nodes[j].times + 1 <= newnodes[j].times {
-                newnodes[j].times = nodes[j].times + 1
-                newnodes[j].log = del(i-1, nodes[j].log)
+            if (nodes[j].times + 1 < newnodes[j].times) {
+                newnodes[j].times = nodes[j].times + 1;
+                newnodes[j].log = del(i-1, nodes[j].log);
             }
             // same
-            if a[i-1] == s[j][k-1] {
-                for _, l := range prevgraph[j] {
-                    if nodes[l].times < newnodes[j].times {
-                        newnodes[j].times = nodes[l].times
-                        newnodes[j].log = nodes[l].log
+            if (a[i-1] == s[j][k-1]) {
+                for (int l : prevgraph[j]) {
+                    if (nodes[l].times < newnodes[j].times) {
+                        newnodes[j].times = nodes[l].times;
+                        newnodes[j].log = nodes[l].log;
                     }
                 }
             } else { // sub
-                for _, l := range prevgraph[j] {
-                    if nodes[l].times + 1 < newnodes[j].times {
-                        newnodes[j].times = nodes[l].times + 1
-                        newnodes[j].log = sub(i-1, s[j][k-1], nodes[l].log)
+                for (int l : prevgraph[j]) {
+                    if (nodes[l].times + 1 < newnodes[j].times) {
+                        newnodes[j].times = nodes[l].times + 1;
+                        newnodes[j].log = sub(i-1, s[j][k-1], nodes[l].log);
                     }
                 }
             }
             // add node back to seq
-            if newnodes[j].times < p { // max is (p-1), or it must be incorrect
-                nodeseqNext = append(nodeseqNext, j)
-            } else if newnodes[j].times == p {
-                pending = append(pending, j)
+            if (newnodes[j].times < p) { // max is (p-1), or it must be incorrect
+                nodeseqNext.push_back(j);
+            } else if (newnodes[j].times == p) {
+                pending.push_back(j);
             } else { // newnodes[j].times == p + 1
-                pendingNext = append(pendingNext, j)
+                pendingNext.push_back(j);
             }
         }
         // save back!
-        nodeseqNext = append(nodeseqNext, pending...)
-        nodeseqNext = append(nodeseqNext, pendingNext...)
-        nodeseq, nodeseqNext, pending, pendingNext = nodeseqNext, nodeseq[:0], pending[:0], pendingNext[:0]
-        pseq := 0
-        pend := pending
-        for j := 0; j < n; j++ {marked[j] = false}
-        for pseq < n {
-            for pseq < n && marked[nodeseq[pseq]] { pseq++ }
-            if pseq == n { break }
-            j := nodeseq[pseq]
-            if len(pend) > 0 && newnodes[pend[0]].times < newnodes[j].times {
-                j = pend[0]
-                pend = pend[1:]
+        nodeseq.resize(0);
+        nodeseq.insert(nodeseq.end(), nodeseqNext.begin(), nodeseqNext.end());
+        nodeseq.insert(nodeseq.end(), pending.begin(), pending.end());
+        nodeseq.insert(nodeseq.end(), pendingNext.begin(), pendingNext.end());
+        nodeseqNext.resize(0);
+        pending.resize(0);
+        pendingNext.resize(0);
+        int pseq = 0;
+        auto pend = pending.begin();
+        for (int j = 0; j < n; j++) marked[j] = false;
+        while (pseq < n) {
+            while (pseq < n && marked[nodeseq[pseq]]) pseq++;
+            if (pseq == n) break;
+            int j = nodeseq[pseq];
+            if (pend != pending.end() && newnodes[*pend].times < newnodes[j].times) {
+                j = *pend++;
             } else {
-                pseq++
+                pseq++;
             }
-            nodeseqNext = append(nodeseqNext, j)
+            nodeseqNext.push_back(j);
+            if (newnodes[j].times == UB) continue;
             // ins
             // it must be correct beacuse all nodes that has less than p dist has already been processed
-            for _, g := range graph[j] {
-                if newnodes[j].times + 1 < newnodes[g].times {
-                    newnodes[g].times = newnodes[j].times + 1
-                    newnodes[g].log = ins(i, s[g][k-1], newnodes[j].log)
-                    pend = append(pend, g)
-                    marked[g] = true
+            for (int g : graph[j]) {
+                if (newnodes[j].times + 1 < newnodes[g].times) {
+                    newnodes[g].times = newnodes[j].times + 1;
+                    newnodes[g].log = ins(i, s[g][k-1], newnodes[j].log);
+                    pending.push_back(g);
+                    marked[g] = true;
                 }
             }
         }
         // save back!
-        nodeseqNext = append(nodeseqNext, pend...)
-        nodeseq, nodeseqNext, pending, pendingNext = nodeseqNext, nodeseq[:0], pending[:0], pendingNext[:0]
-        for j := 0; j < n; j++ {
-            nodes[j].times = newnodes[j].times
-            nodes[j].log = newnodes[j].log
-            newnodes[j].times = -1
-            newnodes[j].log = nil
+        nodeseq.resize(0);
+        nodeseq.insert(nodeseq.end(), nodeseqNext.begin(), nodeseqNext.end());
+        nodeseq.insert(nodeseq.end(), pend, pending.end());
+        nodeseqNext.resize(0);
+        pending.resize(0);
+        for (int j = 0; j < n; j++) {
+            nodes[j].times = newnodes[j].times;
+            nodes[j].log = newnodes[j].log;
+            newnodes[j].times = UB;
+            newnodes[j].log = log_nil;
         }
     }
-    return nodeseq[0]
+    return nodeseq[0];
 }
 
-func updatePreWork(j, i int) {
-    if nodes[j].preWork == nil {
-        return
+void updatePreWork(int j, int i) {
+    if (nodes[j].preWork.size() == 0) {
+        return;
     }
-    preWork := nodes[j].preWork
-    if i == 0 {
-        preWork[0].times = 0
-        preWork[0].log = nil
-        for t := 1; t <= k; t++ {
-            preWork[t].times = preWork[t-1].times + 1
-            preWork[t].log = ins(0, s[j][t-1], preWork[t-1].log)
+    auto &preWork = nodes[j].preWork;
+    if (i == 0) {
+        preWork[0].times = 0;
+        preWork[0].log = log_nil;
+        for (int t = 1; t <= k; t++) {
+            if (preWork[t-1].times + 1 < UB) {
+                preWork[t].times = preWork[t-1].times + 1;
+                preWork[t].log = ins(0, s[j][t-1], preWork[t-1].log);
+            } else {
+                preWork[t].times = UB;
+                preWork[t].log = log_nil;
+            }
         }
     } else {
-        for t := k; t > 0; t-- {
-            if a[i-1] == s[j][t-1] {
-                preWork[t].times = preWork[t-1].times
-                preWork[t].log = preWork[t-1].log
-            } else if preWork[t-1].times <= preWork[t].times {
-                preWork[t].times = preWork[t-1].times + 1
-                preWork[t].log = sub(i-1, s[j][t-1], preWork[t-1].log)
+        for (int t = k; t > 0; t--) {
+            if (a[i-1] == s[j][t-1]) {
+                preWork[t].times = preWork[t-1].times;
+                preWork[t].log = preWork[t-1].log;
+            } else if (preWork[t-1].times + 1 < UB && preWork[t-1].times < preWork[t].times) {
+                preWork[t].times = preWork[t-1].times + 1;
+                preWork[t].log = sub(i-1, s[j][t-1], preWork[t-1].log);
             } else {
-                preWork[t].times++
-                preWork[t].log = del(i-1, preWork[t].log)
+                if (preWork[t].times + 1 < UB) {
+                    preWork[t].times++;
+                    preWork[t].log = del(i-1, preWork[t].log);
+                } else {
+                    preWork[t].times = UB;
+                    preWork[t].log = log_nil;
+                }
             }
         }
-        preWork[0].times++
-        preWork[0].log = del(i-1, preWork[0].log)
-        for t := 1; t <= k; t++ {
-            if preWork[t-1].times + 1 <= preWork[t].times {
-                preWork[t].times = preWork[t-1].times + 1
-                preWork[t].log = ins(i, s[j][t-1], preWork[t-1].log)
+        if (preWork[0].times + 1 < UB) {
+            preWork[0].times++;
+            preWork[0].log = del(i-1, preWork[0].log);
+        } else {
+            preWork[0].times = UB;
+            preWork[0].log = log_nil;
+        }
+        for (int t = 1; t <= k; t++) {
+            if (preWork[t-1].times + 1 < preWork[t].times) {
+                preWork[t].times = preWork[t-1].times + 1;
+                preWork[t].log = ins(i, s[j][t-1], preWork[t-1].log);
             }
         }
     }
 }
 
-func main() {
-    Scanln(&a)
-    Scanln(&n)
-    for i := 0; i < n; i++ {
-        Scanln(&s[i])
+void addEdge(int i, int j);
+void makeGraph();
+int main() {
+    cin >> a >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
     }
-    k = len(s[0])
-    makeGraph()
-    j := doWork()
-    list := conv(a)
-    nodes[j].log.Do(list)
-    Println(unconv(list))
-    Printf("%d\n", nodes[j].times)
-    nodes[j].log.Print()
+    k = s[0].size();
+    makeGraph();
+    int j = doWork();
+    printf("%d\n", nodes[j].times);
+    return 0;
 }
 
-func makeGraph() {
-    var t = make(map[string][]int)
-    for i := 0; i < n; i++ {
-        key := s[i][:k-1]
-        t[key] = append(t[key], i)
+void makeGraph() {
+    unordered_map<string, vector<int>> t;
+    for (int i = 0; i < n; i++) {
+        string key = s[i].substr(0, k-1);
+        t[key].push_back(i);
     }
-    for i := 0; i < n; i++ {
-        key := s[i][1:]
-        for _, j := range t[key] {
-            addEdge(i, j)
+    for (int i = 0; i < n; i++) {
+        string key = s[i].substr(1);
+        for (int j : t[key]) {
+            addEdge(i, j);
         }
     }
 }
 
-func addEdge(i, j int) {
-    graph[i] = append(graph[i], j)
-    prevgraph[j] = append(prevgraph[j], i)
+void addEdge(int i, int j) {
+    graph[i].push_back(j);
+    prevgraph[j].push_back(i);
 }
 
